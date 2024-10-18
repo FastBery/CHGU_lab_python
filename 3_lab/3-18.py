a79e2c0b8d910b32b0d25b0be671a290bb854ff5
FastBery <112929969+FastBery@users.noreply.github.com>
2024-10-18 10:12:50 +0300

    Update 3-18.py

--- 3_lab/3-18.py
+++ 3_lab/3-18.py
@@ -5,16 +5,16 @@ date[0] -= 25
 
 filename = "Programmers.txt"
 data_file = open("./needed_data.txt", "w")
-
+print(date)
 try:
     with open("./" + filename, 'r') as file:
         for i in file.readlines():
             birth_date = [int(j) for j in i.split()[5].split('.')]
             if birth_date[0] > date[0]:
                 data_file.write(i)
-            elif birth_date[0] == date[0] and birth_date[1] < date[1]:
+            elif birth_date[0] == date[0] and birth_date[1] > date[1]:
                 data_file.write(i + '\n')
-            elif birth_date[0] == date[0] and birth_date[1] < date[1] and birth_date[1] == date[1] and birth_date[2] < date[2]:
+            elif birth_date[0] == date[0] and birth_date[1] == date[1] and birth_date[2] > date[2]:
                 data_file.write(i + '\n')
 except:
     print('No such file')
